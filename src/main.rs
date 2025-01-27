6d8109234852d4af19b5217038321304d8062ebe
kolsolv <kolsolv@gmail.com>
2025-01-27 16:32:36 +0400

    Create shared provider

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use alloy::rpc::types::{BlockNumberOrTag, BlockTransactionsKind};
 use alloy::transports::http::{Client, Http};
 use dotenv::dotenv;
 use eyre::Result;
+use once_cell::sync::Lazy;
 use std::env;
 use std::sync::Arc;
 use tokio::sync::Mutex;
@@ -10,23 +11,25 @@ use tokio::time::{self, Duration};
 
 // TODO: Replace expects in requests with something no causing panic
 
+static PROVIDER: Lazy<Arc<RootProvider<Http<Client>>>> = Lazy::new(|| {
+    let rpc_url = env::var("NODE_URL")
+        .expect("Cannot find NODE_URL")
+        .parse()
+        .expect("Failed to parse NODE_URL");
+    let provider = ProviderBuilder::new().on_http(rpc_url);
+    Arc::new(provider)
+});
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv().ok();
 
-    let rpc_url = env::var("NODE_URL")
-        .expect("Cannot find NODE_URL")
-        .parse()?;
-
-    let provider = ProviderBuilder::new().on_http(rpc_url);
-    let cold_start_blocks = cold_start(provider.clone()).await.unwrap_or([].to_vec());
+    let cold_start_blocks = cold_start().await.unwrap_or([].to_vec());
     let blocks = Arc::new(Mutex::new(cold_start_blocks.clone()));
-    let mut interval = time::interval(Duration::from_secs(10));
+    let mut interval = time::interval(Duration::from_secs(5));
 
     // Main block watcher
     loop {
-        interval.tick().await;
-
         // Lock the shared state
         let mut blocks = blocks.lock().await;
         let last_block_number = if blocks.len() > 0 {
@@ -40,7 +43,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         };
         println!("Last block number: {}", last_block_number);
 
-        let new_block = fetch_new_block(provider.clone(), last_block_number).await;
+        let new_block = fetch_new_block(last_block_number).await;
         match new_block {
             Some(block) => {
                 println!("New block: {}", block.header.number);
@@ -50,15 +53,14 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             }
             None => {}
         }
+        interval.tick().await;
     }
 }
 
-async fn cold_start(provider: RootProvider<Http<Client>>) -> Result<Vec<alloy::rpc::types::Block>> {
-    let last_finalized_block =
-        get_block_by_number_or_tag(provider.clone(), BlockNumberOrTag::Finalized).await?;
+async fn cold_start() -> Result<Vec<alloy::rpc::types::Block>> {
+    let last_finalized_block = get_block_by_number_or_tag(BlockNumberOrTag::Finalized).await?;
 
-    let latest_block =
-        get_block_by_number_or_tag(provider.clone(), BlockNumberOrTag::Latest).await?;
+    let latest_block = get_block_by_number_or_tag(BlockNumberOrTag::Latest).await?;
 
     let start_block_number = last_finalized_block.header.number - 11;
     let end_block_number = latest_block.header.number - 1;
@@ -66,25 +68,21 @@ async fn cold_start(provider: RootProvider<Http<Client>>) -> Result<Vec<alloy::r
     let mut blocks = vec![];
 
     for block_number in start_block_number..=end_block_number {
-        let block =
-            get_block_by_number_or_tag(provider.clone(), BlockNumberOrTag::Number(block_number))
-                .await?;
+        let block = get_block_by_number_or_tag(BlockNumberOrTag::Number(block_number)).await?;
         blocks.push(block);
     }
     return Ok(blocks);
 }
 
-async fn fetch_new_block(
-    provider: RootProvider<Http<Client>>,
-    last_block_number: u64,
-) -> Option<alloy::rpc::types::Block> {
+async fn fetch_new_block(last_block_number: u64) -> Option<alloy::rpc::types::Block> {
+    let provider = &*PROVIDER;
     let latest_block_number = provider
         .get_block_number()
         .await
         .expect("Failed to fetch block number");
 
     if latest_block_number > last_block_number {
-        let block = get_block_by_number_or_tag(provider, BlockNumberOrTag::Latest).await;
+        let block = get_block_by_number_or_tag(BlockNumberOrTag::Latest).await;
 
         if block.is_ok() {
             return Some(block.unwrap());
@@ -95,10 +93,8 @@ async fn fetch_new_block(
     return None;
 }
 
-async fn get_block_by_number_or_tag(
-    provider: RootProvider<Http<Client>>,
-    tag: BlockNumberOrTag,
-) -> Result<alloy::rpc::types::Block> {
+async fn get_block_by_number_or_tag(tag: BlockNumberOrTag) -> Result<alloy::rpc::types::Block> {
+    let provider = &*PROVIDER;
     let block = provider
         .get_block_by_number(tag, BlockTransactionsKind::Hashes)
         .await
