1d88b394616f415d0f91c7c18eadd5b927a31af2
kolsolv <kolsolv@gmail.com>
2025-01-27 20:33:17 +0400

    Move blocks functions to module

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,15 @@
-use alloy::providers::{Provider, ProviderBuilder, RootProvider};
-use alloy::rpc::types::{Block, BlockNumberOrTag, BlockTransactionsKind};
-use alloy::transports::http::{Client, Http};
-use dotenv::dotenv;
 use eyre::Result;
-use once_cell::sync::Lazy;
-use std::env;
 use std::sync::Arc;
 use tokio::sync::Mutex;
 use tokio::time::{self, Duration};
 
-// TODO: Replace expects in requests with something no causing panic
+mod blocks;
+use blocks::{cold_start, fetch_new_block};
 
-static PROVIDER: Lazy<Arc<RootProvider<Http<Client>>>> = Lazy::new(|| {
-    let rpc_url = env::var("NODE_URL")
-        .expect("Cannot find NODE_URL")
-        .parse()
-        .expect("Failed to parse NODE_URL");
-    let provider = ProviderBuilder::new().on_http(rpc_url);
-    Arc::new(provider)
-});
+// TODO: Replace expects in requests with something no causing panic
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    dotenv().ok();
-
     let blocks = Arc::new(Mutex::new([].to_vec()));
     let shared_blocks = Arc::clone(&blocks);
 
@@ -59,60 +45,4 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
         interval.tick().await;
     }
-}
-
-async fn cold_start(
-    blocks: Arc<Mutex<Vec<Block>>>,
-) -> Result<(), Box<dyn std::error::Error + Send>> {
-    let last_finalized_block = get_block_by_number_or_tag(BlockNumberOrTag::Finalized)
-        .await
-        .expect("Can't fetch last finalized block");
-    let latest_block = get_block_by_number_or_tag(BlockNumberOrTag::Latest)
-        .await
-        .expect("Can't fetch latest block");
-
-    let start_block_number = last_finalized_block.header.number - 11;
-    let end_block_number = latest_block.header.number - 1;
-
-    let mut new_blocks = vec![];
-
-    for block_number in start_block_number..=end_block_number {
-        let block = get_block_by_number_or_tag(BlockNumberOrTag::Number(block_number))
-            .await
-            .expect("Can't fetch block");
-        new_blocks.push(block);
-    }
-    blocks.lock().await.splice(0..0, new_blocks.iter().cloned());
-    return Ok(());
-}
-
-async fn fetch_new_block(last_block_number: u64) -> Option<alloy::rpc::types::Block> {
-    let provider = &*PROVIDER;
-    let latest_block_number = provider
-        .get_block_number()
-        .await
-        .expect("Failed to fetch block number");
-
-    if latest_block_number > last_block_number {
-        let block = get_block_by_number_or_tag(BlockNumberOrTag::Latest).await;
-
-        if block.is_ok() {
-            return Some(block.unwrap());
-        }
-        return None;
-    }
-
-    return None;
-}
-
-async fn get_block_by_number_or_tag(tag: BlockNumberOrTag) -> Result<alloy::rpc::types::Block> {
-    let provider = &*PROVIDER;
-
-    let block = provider
-        .get_block_by_number(tag, BlockTransactionsKind::Hashes)
-        .await
-        .expect("Failed to fetch block")
-        .expect("Block not found");
-
-    return Ok(block);
 }
